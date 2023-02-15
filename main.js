1dcd2f5d31c1ae671b420d5d90a02bc71d86caa9
Yasmeen Dao <dao.ya@northeastern.edu>
2023-02-15 12:21:55 -0500

    update

--- main.js
+++ main.js
@@ -1,13 +1,5 @@
 // JS file for hw-04
 
-
-let vals = document.getElementById("data");
-let vals_children = vals.children;
-
-// convert val children to array
-let circle_array = Array.from(vals_children); 
-
-
 // function for changing border of shape and printing point
 function circleClick()
 {
@@ -23,27 +15,42 @@ function circleClick()
 
 } //end of function
 
-//  add event handler for each circle 
-for(let i = 0; i<circle_array.length; i++)
-{
-	circle_array[i].addEventListener("click",circleClick);
-}
-
-
 //  add point to grid
 function addPoint()
 {
 	// add new points
+	// reverse the arithmetic
 	 let xcord = (document.getElementById("x-option").value * 40) + 10
 	 let ycord = ((9 - document.getElementById("y-option").value) * 40) + 30
 	 let r = 8; 
 	 // add point to graph
 	 let g = document.getElementById("data");
+	 // access the circles from the data id
 	 g.innerHTML += "<circle "+" cx=" + xcord + " cy=" + ycord +  " r=" + r + "></circle>"
+	 // call event listener function
+	 listeners()
 }
 
-
+// Event listener for point
 document.getElementById("addCircle").addEventListener("click",addPoint);
 
+// event listener function
+function listeners() 
+{
+	// get the id from the index
+	let vals = document.getElementById("data");
+	// create value children
+	let vals_children = vals.children;
 
+	// convert val children to array
+	let circle_array = Array.from(vals_children); 
 
+     //add event handler for each circle 
+	for(let i = 0; i<circle_array.length; i++)
+	{
+		circle_array[i].addEventListener("click",circleClick);
+	}
+
+}
+//call event listener
+listeners()
